1a92340e5babb0ad11ddc81b8826a2897aa7ec5b
Florencia D'Andrea <florencia.dandrea@gmail.com>
2019-08-24 15:36:48 -0300

    library added

--- exercises/solution_01_07_1.R
+++ exercises/solution_01_07_1.R
@@ -1,10 +1,10 @@
 library(tidyverse)
+library(caret)
 
 car_train <- readRDS("data/c1_training_full.rds")
 car_test <- readRDS("data/c1_testing_full.rds")
 
-# Load caret
-data/(caret)
+library(caret)
 
 # Train a linear regression model
 fit_lm <- train(log(MPG) ~ ., 
