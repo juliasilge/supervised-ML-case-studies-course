7c149a7ae49a41f5b9e071d698c0259ace8bc3a2
Florencia D'Andrea <florencia.dandrea@gmail.com>
2019-08-24 15:43:22 -0300

    mismatch with exercise file fixed

--- exercises/solution_01_07_1.R
+++ exercises/solution_01_07_1.R
@@ -3,6 +3,9 @@ library(tidyverse)
 car_train <- readRDS("data/c1_training_full.rds")
 car_test <- readRDS("data/c1_testing_full.rds")
 
+# Load caret
+___
+
 # Train a linear regression model
 fit_lm <- train(log(MPG) ~ ., 
                 method = "lm", 
