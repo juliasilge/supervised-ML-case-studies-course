ef12d5254429d0932a21d85f704cb9562fe830f1
Julia Silge <julia.silge@gmail.com>
2019-08-29 10:57:12 -0600

    Let's use this name for dataset instead

--- exercises/exc_03_12_1.R
+++ exercises/exc_03_12_1.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 library(caret)
 
-training <- readRDS("data/c3_training_two_percent.rds")
+vote_train <- readRDS("data/c3_training_two_percent.rds")
 
 # Logistic regression
 vote_glm <- train(turnout16_2016 ~ ., 
                   method = "glm", family = "binomial",
-                  data = training,
+                  data = vote_train,
                   trControl = trainControl(method = ___,
                                            repeats = ___,
                                            sampling = "up"))
--- exercises/solution_03_12_2.R
+++ exercises/solution_03_12_2.R
@@ -1,11 +1,11 @@
 library(tidyverse)
 library(caret)
 
-training <- readRDS("data/c3_training_two_percent.rds")
+vote_train <- readRDS("data/c3_training_two_percent.rds")
 
 # Random forest
 vote_rf <- train(turnout16_2016 ~ ., method = "rf", 
-                 data = training,
+                 data = vote_train,
                  trControl = trainControl(method = "repeatedcv",
                                           repeats = 2,
                                           sampling = "up"))
