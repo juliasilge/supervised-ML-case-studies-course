5292286d91d7582d0fe268b0a832a7d7498a72d8
Florencia D'Andrea <florencia.dandrea@gmail.com>
2019-08-29 08:13:31 -0300

    name dataset

--- exercises/exc_03_12_2.R
+++ exercises/exc_03_12_2.R
@@ -1,7 +1,7 @@
 library(tidyverse)
 library(caret)
 
-vote_train <- readRDS("data/c3_training_two_percent.rds")
+training <- readRDS("data/c3_training_two_percent.rds")
 
 # Random forest
 vote_rf <- train(turnout16_2016 ~ ., method = "rf", 
