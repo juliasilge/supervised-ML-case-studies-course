e8f46eb65b4583d3848399093621f669d586de48
Florencia D'Andrea <florencia.dandrea@gmail.com>
2019-08-29 08:12:36 -0300

    Update solution_03_12_1.R

--- exercises/solution_03_12_1.R
+++ exercises/solution_03_12_1.R
@@ -1,12 +1,12 @@
 library(tidyverse)
 library(caret)
 
-vote_train <- readRDS("data/c3_training_two_percent.rds")
+training <- readRDS("data/c3_training_two_percent.rds")
 
 # Logistic regression
 vote_glm <- train(turnout16_2016 ~ ., 
                   method = "glm", family = "binomial",
-                  data = vote_train,
+                  data = training,
                   trControl = trainControl(method = "repeatedcv",
                                            repeats = 2,
                                            sampling = "up"))
