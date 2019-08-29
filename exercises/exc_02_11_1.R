2fd6509958a9199dddea7fac280f30fcc2788cb1
Julia Silge <julia.silge@gmail.com>
2019-08-29 10:47:18 -0600

    Don't need this testing dataset. Closes #10.

--- exercises/exc_02_11_1.R
+++ exercises/exc_02_11_1.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(caret)
 
 stack_train <- readRDS("data/c2_training_one_percent.rds")
-stack_test <- readRDS("data/c2_testing_one_percent.rds")
 
 # Build a logistic regression model
 stack_glm <- train(Remote ~ ., ___, ___,
--- exercises/solution_02_11_2.R
+++ exercises/solution_02_11_2.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(caret)
 
 stack_train <- readRDS("data/c2_training_one_percent.rds")
-stack_test <- readRDS("data/c2_testing_one_percent.rds")
 
 # Build a random forest model
 stack_rf <- train(Remote ~ ., method = "rf", 
