80a1e0b6812ace93e75855ad781b4ca670f0fc52
Florencia D'Andrea <florencia.dandrea@gmail.com>
2019-09-01 11:18:06 -0300

    datasets

--- exercises/exc_01_12_2.R
+++ exercises/exc_01_12_2.R
@@ -1,4 +1,10 @@
+library(caret)
 library(tidyverse)
+library(yardstick)
+
+car_test <- readRDS("data/c1_testing_full.rds")
+cars_lm_bt <- readRDS("data/cars_lm_bt.rds")
+cars_rf_bt <- readRDS("data/cars_rf_bt.rds")
 
 results <- car_test %>%
     mutate(`Linear regression` = predict(cars_lm_bt, car_test),
