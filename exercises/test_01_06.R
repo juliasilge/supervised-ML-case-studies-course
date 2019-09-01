cf7ec0ee13eb786b1ba638305380776b487e9fa8
Florencia D'Andrea <florencia.dandrea@gmail.com>
2019-09-01 10:05:24 -0300

    Update test_01_06.R

--- exercises/test_01_06.R
+++ exercises/test_01_06.R
@@ -1,2 +1,2 @@
-test <- function(){success('Now you have training and testing data sets, ready for machine learning! Remember that it is important to set a seed for reproducibility when creating training-testing splits.!')}
+test <- function(){success('Now you have training and testing data sets, ready for machine learning! Remember that it is important to set a seed for reproducibility when creating training-testing splits!')}
 
