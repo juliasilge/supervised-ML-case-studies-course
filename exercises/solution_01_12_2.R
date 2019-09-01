88e6ae4300acac4c6a88aa30299ce5704eeeb04e
Florencia D'Andrea <florencia.dandrea@gmail.com>
2019-09-01 11:11:50 -0300

    results

--- exercises/solution_01_12_2.R
+++ exercises/solution_01_12_2.R
@@ -1,5 +1,9 @@
 library(tidyverse)
 
+results <- car_test %>%
+    mutate(`Linear regression` = predict(cars_lm_bt, car_test),
+           `Random forest` = predict(cars_rf_bt, car_test))
+
 results %>%
     gather(Method, Result, `Linear regression`:`Random forest`) %>%
     ggplot(aes(log(MPG), Result, color = Method)) +
