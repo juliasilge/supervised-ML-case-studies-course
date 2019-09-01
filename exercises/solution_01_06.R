5ccf402bd01c26de2af9df791b3f05d57f11fb0d
Florencia D'Andrea <florencia.dandrea@gmail.com>
2019-09-01 10:58:30 -0300

    typo

--- exercises/solution_01_06.R
+++ exercises/solution_01_06.R
@@ -7,7 +7,7 @@ library(rsample)
 # Split the data into training and test sets
 set.seed(1234)
 car_split <- car_vars %>%
-    initial_split(prop = 0.8, strata = `Transmission`)
+    initial_split(prop = 0.8, strata = "Transmission")
 
 car_train <- training(car_split)
 car_test <- testing(car_split)
