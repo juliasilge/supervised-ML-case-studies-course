5ecd385afab963244aaddbf6d52d9fd00baf8ddd
Florencia D'Andrea <florencia.dandrea@gmail.com>
2019-09-07 16:59:17 -0300

    Update exc_02_03_1.R

--- exercises/exc_02_03_1.R
+++ exercises/exc_02_03_1.R
@@ -3,3 +3,11 @@ stackoverflow <- read_csv("data/stackoverflow.csv")
 
 # Print stackoverflow
 ___
+
+# First count for Remote
+stackoverflow %>% 
+    count(___, sort = TRUE)
+
+# then count for Country
+stackoverflow %>% 
+    ___(___, sort = TRUE)
