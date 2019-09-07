b925115b0bcb3d82720e48ee0b91651dd8012aeb
Florencia D'Andrea <florencia.dandrea@gmail.com>
2019-09-07 09:53:09 -0300

    trying tests 1

--- exercises/solution_02_03_1.R
+++ exercises/solution_02_03_1.R
@@ -3,11 +3,3 @@ stackoverflow <- read_csv("data/stackoverflow.csv")
 
 # Print stackoverflow
 stackoverflow
-
-# First count for Remote
-stackoverflow %>% 
-    count(Remote, sort = TRUE)
-
-# then count for Country
-stackoverflow %>% 
-    count(Country, sort = TRUE)
