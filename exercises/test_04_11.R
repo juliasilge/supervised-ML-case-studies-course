17736e1c4a3c7b8272baa170fc68b3ad266a6f40
Julia Silge <jsilge@stackoverflow.com>
2019-09-19 22:03:20 -0600

    EOF issues allllll day long

--- exercises/test_04_11.R
+++ exercises/test_04_11.R
@@ -1,2 +1,1 @@
 test <- function(){success('Great job! In this case study, you will evaluate this model, plus two kinds of gradient boosting models.')}
-
--- exercises/test_04_13.R
+++ exercises/test_04_13.R
@@ -1,2 +1,1 @@
 test <- function(){success('Excellent! Notice that the `gbm` gradient boosting regression model performed best, at least with the default tuning parameters we used in training.')}
-
