04e3c83e22427a6adb7586db6fb4cc1e168346ae
Julia Silge <jsilge@stackoverflow.com>
2019-10-05 20:46:45 -0600

    Chapter 1 exercise 12 needs to clarify where log was taken. Closes #35.

--- exercises/exc_01_12_1.R
+++ exercises/exc_01_12_1.R
@@ -7,7 +7,8 @@ cars_lm_bt <- readRDS("data/cars_lm_bt.rds")
 cars_rf_bt <- readRDS("data/cars_rf_bt.rds")
 
 results <- car_test %>%
-    ___(`Linear regression` = predict(cars_lm_bt, car_test),
+    ___(MPG = log(MPG),
+        `Linear regression` = predict(cars_lm_bt, car_test),
            `Random forest` = predict(cars_rf_bt, car_test))
 
 metrics(results, ___ = MPG, ___ = `Linear regression`)
